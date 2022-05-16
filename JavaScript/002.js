cb24dad807f129e241c7a6b1fdac5f1b7017746c
Soohyeon Kim <tngusglaso@gmail.com>
2022-05-16 16:21:58 +0900

    수업 변동사항

--- JavaScript/002.js
+++ JavaScript/002.js
@@ -1,5 +1,4 @@
-document.getElementById('one').innerHTML =
-  'hello <strong>world document</strong>';
-console.log('hello world console');
-window.alert('hello world alert');
-document.write('hello!!');
+document.getElementById("one").innerHTML = "hello <strong>world document</strong>";
+console.log("hello world console");
+window.alert("hello world alert");
+document.write("hello!!");
